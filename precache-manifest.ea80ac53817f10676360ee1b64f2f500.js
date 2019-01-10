3a414af23feceaffc838af90538ea825d4c1e1e7
Edward Tanoto <ed27tanoto@gmail.com>
2019-01-10 21:11:16 +0700

    Updates

---
+++ precache-manifest.ea80ac53817f10676360ee1b64f2f500.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e7ea501e7b8f22ce0b7",
+    "url": "/weatherapp/static/js/runtime~main.0e7ea501.js"
+  },
+  {
+    "revision": "d08e1fae28762f221005",
+    "url": "/weatherapp/static/js/main.d08e1fae.chunk.js"
+  },
+  {
+    "revision": "bcf5c7583f5fcdc0db59",
+    "url": "/weatherapp/static/js/1.bcf5c758.chunk.js"
+  },
+  {
+    "revision": "d08e1fae28762f221005",
+    "url": "/weatherapp/static/css/main.5f6757ab.chunk.css"
+  },
+  {
+    "revision": "bcf5c7583f5fcdc0db59",
+    "url": "/weatherapp/static/css/1.9f931ca6.chunk.css"
+  },
+  {
+    "revision": "e8f58f0f96776106da766a6117d4e424",
+    "url": "/weatherapp/index.html"
+  }
+];
